6c3581f93c9d25c47a4a40a58d4defdc0f5a24e5
Sasha Rogers <hnrogers10@gmail.com>
2024-08-14 15:09:55 -0400

    adding reset/end stuff, but resetting TDs not working

--- testtest.js
+++ testtest.js
@@ -1,32 +1,70 @@
 let nowPlaying = false;
 let turnCount = 0;
+let matchEnd = true;
 
 document.getElementById("thetable").addEventListener("click", function(e) {
     
+    let arrElement = 0;
+
     if (nowPlaying === true) {
 
         let cell = e.target.closest('td');
         let row = cell.parentElement;
-        let arrElement = "";
 
         switch (row.rowIndex) {
             case 0:
-                arrElement = cell.cellIndex;
+                arrElement += cell.cellIndex;
                 break;
             case 1:
-                arrElement = cell.cellIndex + 3;
+                arrElement += cell.cellIndex + 3;
                 break;
             case 2:
-                arrElement = cell.cellIndex + 6;
+                arrElement += cell.cellIndex + 6;
                 break;
         }
 
-        // cell.innerText = "X";
         if (cell.innerText !== "X" && cell.innerText !== "O") {
             cell.innerText = turnCount % 2 === 0 ? "O" : "X";
+            gameboard.board[arrElement] = turnCount % 2 === 0 ? "O" : "X";
+            console.log(gameboard.board[arrElement]);
             turnCount++;
         }
     }
+    tempStop();
+    /*
+    if (turnCount >= 5) {
+
+        switch (arrElement) {
+            case 0:
+                matchEnd = checkWin(gameboard, logic.zero, turnCounter);
+                break;
+            case 1:
+                matchEnd = checkWin(gameboard, logic.one, turnCounter);
+                break;
+            case 2:
+                matchEnd = checkWin(gameboard, logic.two, turnCounter);
+                break;
+            case 3:
+                matchEnd = checkWin(gameboard, logic.three, turnCounter);
+                break;
+            case 4:
+                matchEnd = checkWin(gameboard, logic.four, turnCounter);
+                break;
+            case 5:
+                matchEnd = checkWin(gameboard, logic.five, turnCounter);
+                break;
+            case 6:
+                matchEnd = checkWin(gameboard, logic.six, turnCounter);
+                break;
+            case 7:
+                matchEnd = checkWin(gameboard, logic.seven, turnCounter);
+                break;
+            case 8:
+                matchEnd = checkWin(gameboard, logic.eight, turnCounter);
+                break;
+        }
+    }
+    */
     
 })
 
@@ -34,9 +72,29 @@ document.getElementById("startMatch").addEventListener("click", () => { // start
     nowPlaying = true;
     turnCount = 1;
     document.getElementById("startMatch").disabled = true;
-    alert(nowPlaying);
 })
 
+function tempStop() { // TEMPORARY
+    if (turnCount > 9) {
+        gameboard.reset();
+        turnCount = 0;
+        document.getElementById("startMatch").disabled = false;
+        document.getElementsById("zero").innerText = "";
+
+        
+        /*
+        document.getElementsById("zero").innerText = "";
+        document.getElementsById("one").innerText = "";
+        document.getElementsById("two").innerText = "";
+        document.getElementsById("three").innerText = "";
+        document.getElementsById("four").innerText = "";
+        document.getElementsById("five").innerText = "";
+        document.getElementsById("six").innerText = "";
+        document.getElementsById("seven").innerText = "";
+        document.getElementsById("eight").innerText = "";
+        */
+    }
+}
 
 const logic = (function() {
     const zero = [[0, 1, 2], [0, 4, 8], [0, 3, 6]];
@@ -49,4 +107,12 @@ const logic = (function() {
     const seven = [[6, 7, 8], [1, 4, 7]];
     const eight = [[6, 7, 8], [0, 4, 8], [2, 5, 8]];
     return { zero, one, two, three, four, five, six, seven, eight };
+})();
+
+const gameboard = (function() {
+    let board = ["", "", "", "", "", "", "", "", ""];
+    function reset() {
+        board.forEach((data, index) => board[index] = "");
+    } 
+    return { board, reset };
 })();
